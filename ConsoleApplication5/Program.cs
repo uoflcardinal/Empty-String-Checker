06c5d219ebbed0109ba59d7fae1fac83564d178e
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-23 16:31:37 -0400

    Empty String Checker

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Please enter some text: ");
+            input = Console.ReadLine();
+            Console.WriteLine(check(input));
+        }
+
+        public static bool check(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+    }
+}
